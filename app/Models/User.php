55d5ecbc2c9b7ed35cc91205fa1ff1fa81f8c90f
David Yell <david.yell@asset.tv>
2025-02-04 16:54:46 +0000

    Add larastan

--- app/Models/User.php
+++ app/Models/User.php
@@ -26,7 +26,7 @@ class User extends Authenticatable
     /**
      * The attributes that are mass assignable.
      *
-     * @var array<int, string>
+     * @var list<string>
      */
     protected $fillable = [
         'name',
@@ -37,7 +37,7 @@ class User extends Authenticatable
     /**
      * The attributes that should be hidden for serialization.
      *
-     * @var array<int, string>
+     * @var list<string>
      */
     protected $hidden = [
         'password',
@@ -49,7 +49,7 @@ class User extends Authenticatable
     /**
      * The accessors to append to the model's array form.
      *
-     * @var array<int, string>
+     * @var list<string>
      */
     protected $appends = [
         'profile_photo_url',
